0e49a73f970bac1b118877dbb146b8430bb8f81a
Felipe Mota <felipe.msrb@gmail.com>
2022-10-11 11:16:14 +0100

    atribuinto o atributo onChange no input do campotexto

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,18 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    let valor = ''
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required = {props.obrigatorio} placeholder = {props.placeholder}></input>
+            <input  onChange= {aoDigitado} required = {props.obrigatorio} placeholder = {props.placeholder}></input>
         </div>
     )
 }
