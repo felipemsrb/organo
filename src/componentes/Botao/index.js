9f5bc6978aed4051b308c1f2e15903d35fff4a75
Felipe Mota <felipe.msrb@gmail.com>
2022-10-11 10:28:33 +0100

    projeto organo

---
+++ src/componentes/Botao/index.js
@@ -0,0 +1,14 @@
+import './Botao.css'
+
+function Botao(props) {
+    return(
+        <div>
+            <button className='botao'>
+                {props.children}
+            </button>
+        </div>
+
+    )
+}
+
+export default Botao
