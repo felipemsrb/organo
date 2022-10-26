366cb7dbcc48be6a9d3006b25999f23d0e07ca58
Felipe Mota <felipe.msrb@gmail.com>
2022-10-26 11:30:29 +0100

    criando o componente colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,21 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img
+          src="https://github.com/felipemsrb.png"
+          alt="Foto do colaborador"
+        ></img>
+      </div>
+
+      <div className="rodape">
+        <h4>Felipe Mota</h4>
+        <h5>Programador</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
